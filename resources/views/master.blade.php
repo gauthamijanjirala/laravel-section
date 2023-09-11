ce43145b953c1b35f37bba2703890c9477bcb850
Srikanth <code.srikanthjanjirala@gmail.com>
2023-09-11 18:55:16 +0530

    test

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -88,10 +88,10 @@
 </style>
 <body>
     <div class="navbar">
-      <a href="#"><i class="fa fa-fw fa-home"></i> Home</a> 
-      <a href="#"><i class="fa fa-fw fa-search"></i> Search</a> 
-      <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a> 
-      <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
+        <a href="#"><i class="fa fa-fw fa-home"></i> Home</a> 
+        <a href="#"><i class="fa fa-fw fa-search"></i> Search</a> 
+        <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a> 
+        <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
     </div>
     <div class="sidebar">
         <!-- <a class="active" href="#home">Home</a> -->
